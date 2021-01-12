095b1da9b0938fd316a8cec55213f7d1c03c6c43
Gonzalo Gorgojo <gongorgojo@gmail.com>
2021-01-12 00:58:24 -0300

    Logica 2.0
    Al comenzar le asigna a cada carta una foto y la mantiene hasta terminar el juego. Tambien se agrego que si dos cartas son iguales las esconde.

--- script.js
+++ script.js
@@ -1,64 +1,71 @@
 const $carta = document.querySelectorAll('.carta');
+const $puntaje = document.querySelector('#puntaje');
 
 let imagenes = ["/imgs/chandler.jpeg", "/imgs/joey.jpg", "/imgs/monica.webp", "/imgs/phoebe.jpeg", "/imgs/rachel.webp", "/imgs/ross.webp"];
-let listaClicks = [];
+let listaUrlImagenesClickeadas = [];
+let listaIdsImagenesClickeadas = [];
 let listaImagenCobertora = [];
 let contador = 0;
+let puntaje = 0;
 
 
-function juegaUsuario() {
-    document.onclick = clickUsuario;
+window.onload = function () {
+    ordenarImagenes();
+};
+
+function ordenarImagenes() {
+    imagenes = [...imagenes, ...imagenes]
+    imagenes.sort(() => Math.random() - 0.5);
 }
-juegaUsuario();
+
+document.onclick = clickUsuario;
 
 function clickUsuario(e) {
-    const click = e.target.id;
+    const IMAGEN_CLICKEADA = e.target.id;
     const PRIMER_IMAGEN = e.target.src;
-    listaImagenCobertora.push(PRIMER_IMAGEN)
-    if ((/^[0-9]/).test(click)) {
-        mostrarImagen(click - 1)
-
+    listaIdsImagenesClickeadas.push(IMAGEN_CLICKEADA)
+    listaImagenCobertora.push(PRIMER_IMAGEN);
+    if ((/^[0-9]/).test(IMAGEN_CLICKEADA)) {
+        puntaje++
+        $puntaje.innerHTML = puntaje;
+        mostrarImagen(IMAGEN_CLICKEADA)
     }
 }
 
-function mostrarImagen(numeroImagen) {
-    let numeroAleatorio = ObtenerNumeroAleatorio();
-    $carta[numeroImagen].setAttribute("src", imagenes[numeroAleatorio]);
-    const url = $carta[numeroImagen].src;
-    listaClicks.push(url);
-    comparar(numeroImagen, numeroAleatorio);
+function mostrarImagen(IMAGEN_CLICKEADA) {
+    $carta[IMAGEN_CLICKEADA].setAttribute("src", imagenes[IMAGEN_CLICKEADA]);
+    const URL_IMAGEN = $carta[IMAGEN_CLICKEADA].src;
+    listaUrlImagenesClickeadas.push(URL_IMAGEN);
+    comparar(IMAGEN_CLICKEADA);
 }
 
-function comparar(numeroImagen, numeroAleatorio) {
+function comparar(IMAGEN_CLICKEADA) {
     contador++;
-    console.log(contador)
     if (contador === 1) {
-        $carta[numeroImagen].setAttribute("src", imagenes[numeroAleatorio]);
+        $carta[IMAGEN_CLICKEADA].setAttribute("src", imagenes[IMAGEN_CLICKEADA]);
     }
     if (contador > 1) {
-        if (listaClicks[listaClicks.length - 2] === listaClicks[listaClicks.length - 1]) {
-            bloquearUsuario();
+        if (listaUrlImagenesClickeadas[listaUrlImagenesClickeadas.length - 2] === listaUrlImagenesClickeadas[listaUrlImagenesClickeadas.length - 1]) {
             setTimeout(() => {
-                alert('ganaste')
-            }, 100);
+                $carta[listaIdsImagenesClickeadas[listaIdsImagenesClickeadas.length - 1]].style.visibility = "hidden"
+                $carta[listaIdsImagenesClickeadas[listaIdsImagenesClickeadas.length - 2]].style.visibility = "hidden"
+                compronarGano();
+            }, 200);
         } else {
             setTimeout(() => {
-                $carta[numeroImagen].setAttribute("src", listaImagenCobertora[listaImagenCobertora.length - 1]);
-                $carta[numeroImagen - 1].setAttribute("src", listaImagenCobertora[listaImagenCobertora.length - 2]);
+                $carta[listaIdsImagenesClickeadas[listaIdsImagenesClickeadas.length - 1]].setAttribute("src", listaImagenCobertora[listaImagenCobertora.length - 1]);
+                $carta[listaIdsImagenesClickeadas[listaIdsImagenesClickeadas.length - 2]].setAttribute("src", listaImagenCobertora[listaImagenCobertora.length - 2]);
             }, 200);
         }
         contador = 0;
+
     }
 }
 
-function ObtenerNumeroAleatorio() {
-    const NUMERO_ALEATORIO = Math.floor(Math.random() * imagenes.length);
-    return NUMERO_ALEATORIO;
+/*
+function compronarGano() {
+    if ($carta[].style.visibility === "hidden") {
+        alert('ganaste')
+    }
 }
-
-function bloquearUsuario() {
-    $carta.forEach(function ($boton) {
-        $boton.onclick = function () {
-        };
-    });
-}
+*/
